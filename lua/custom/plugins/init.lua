326dfb8f22dbcb6980926d6722e2c4fec6482bea
Aditya Sunil Kulkarni <adityakulkarni61@gmail.com>
2024-04-02 23:25:23 -0400

    added multiple plugins
    removed navic
    changed barbecue opt and config
    added trouble
    added neodev
    added conform
    added neotree
    added git-conflict

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,55 +5,57 @@
 return {
     {
         "kdheepak/lazygit.nvim",
-        name ="lazygit",
-    	cmd = {
-    	    "LazyGit",
-    	    "LazyGitConfig",
-    	    "LazyGitCurrentFile",
-    	    "LazyGitFilter",
-    	    "LazyGitFilterCurrentFile",
-    	},
+        name = "lazygit",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-	lazy = false,
-	config = function()
-            require("telescope").load_extension("lazygit")
-	    vim.keymap.set('n', '<leader>gc', '<cmd>LazyGit<cr>', {desc = '[G]it [C]ommit'})
-        end,
-    },
-        {
-        "SmiteshP/nvim-navic",
-        name = "nvim-navic",
-        dependencies = {
-            "neovim/nvim-lspconfig"
-        },
         config = function()
-            local navic = require("nvim-navic")
-            local on_attach = function(client, bufnr)
-            if client.server_capabilities.documentSymbolProvider then
-                navic.attach(client, bufnr)
-            end
-            end
-
-        require("lspconfig").clangd.setup {
-            on_attach = on_attach
-        }
+            require("telescope").load_extension("lazygit")
         end,
+        keys = {
+            { "<leader>gc", "<cmd>LazyGit<cr>", desc = "[G]it [C]ommit" }
+        },
     },
-        {
+    {
         "utilyre/barbecue.nvim",
         name = "barbecue",
         version = "*",
         dependencies = {
             "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            create_autocmd = false,
         },
         config = function()
-            require("barbecue.ui").toggle()
+            require("barbecue.ui").toggle(true)
+
+            vim.api.nvim_create_autocmd({
+                    "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+                    "BufWinEnter",
+                    "CursorHold",
+                    "InsertLeave",
+
+                    -- include this if you have set `show_modified` to `true`
+                    "BufModifiedSet",
+                },
+                {
+                    group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+                    callback = function()
+                        require("barbecue.ui").update()
+                    end,
+                })
         end,
     },
-        {
+    {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
@@ -62,5 +64,73 @@ return {
             -- refer to the configuration section below
         }
     },
-
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+        keys = {
+            { "<leader>ta", function() require("trouble").toggle() end,                        desc = "[T]oggle [A]ll Diagnostics" },
+            { "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end, desc = "[T]oggle [W]orkspace Diagnostics" },
+            { "<leader>td", function() require("trouble").toggle("document_diagnostics") end,  desc = "[T]oggle [D]ocuemnt Diagnostics" },
+            { "<leader>tq", function() require("trouble").toggle("quickfix") end,              desc = "[T]oggle [Q]uickfixes" },
+            { "<leader>tl", function() require("trouble").toggle("loclist") end,               desc = "[T]oggle [L]ocallist" },
+            { "<leader>cr", function() require("trouble").toggle("lsp_references") end,        desc = "[C]ode [R]eference below cursor" },
+        },
+    },
+    {
+        "folke/neodev.nvim",
+        opts = {},
+    },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                -- Customize or remove this keymap to your liking
+                "<leader>fb",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+                mode = "",
+                desc = "[F]ormat [B]uffer",
+            },
+        },
+        -- Everything in opts will be passed to setup()
+        opts = {
+            -- Define your formatters
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+            },
+            -- Set up format-on-save
+            format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            -- Customize formatters
+            formatters = {
+                shfmt = {
+                    prepend_args = { "-i", "2" },
+                },
+            },
+        },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        keys = {
+            { "<leader>fo", "<cmd>Neotree position=current<cr>", desc = "[F]iletree [O]pen" },
+            { "<leader>fc", "<cmd>Neotree close<cr>",            desc = "[F]iletree [C]lose" },
+        },
+    },
+    {
+        'akinsho/git-conflict.nvim',
+        version = "1.*",
+        config = true,
+    },
 }
